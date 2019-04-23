f7f649ee413fc6a48ea292cbf785c280fce2283d
LeAnhToan <anh-toan1997@hotmail.com>
2019-04-23 15:27:17 +0200

    Bug inputSeed button resolved

--- app/src/main/java/com/example/dungeon/AccueilActivity.java
+++ app/src/main/java/com/example/dungeon/AccueilActivity.java
@@ -13,8 +13,9 @@ import android.widget.EditText;
 
 
 public class AccueilActivity extends Activity {
-     Button mButton;
+    Button mButton;
     EditText mEdit;
+    String mEditString;
 
 
         /** Called when the activity is first created. */
@@ -22,32 +23,36 @@ public class AccueilActivity extends Activity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_accueil);
-
-            mButton = (Button)findViewById(R.id.button_seed);
-            mEdit   = (EditText)findViewById(R.id.editSeed);
-            SharedPreferences test;
-            final SharedPreferences.Editor editor;
-            test = PreferenceManager.getDefaultSharedPreferences(this);
-            editor = test.edit();
-
-
-            mButton.setOnClickListener(
-                    new View.OnClickListener()
-                    {
-                        public void onClick(View view)
-                        {
-                            Log.v("EditText", mEdit.getText().toString());
-                            editor.putLong("newSeed",Long.valueOf(mEdit.getText().toString()));
-                            editor.commit();
-                        }
-                    });
-
         }
 
         public void main(View v) {
 
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+        }
+
+        public void seed(View v) {
+
+            mButton = (Button)findViewById(R.id.button_seed);
+            mEdit   = (EditText)findViewById(R.id.editSeed);
+
+            SharedPreferences test;
+            final SharedPreferences.Editor editor;
+            test = PreferenceManager.getDefaultSharedPreferences(this);
+            editor = test.edit();
+
+            mEditString = mEdit.getText().toString() ;
+            if (!mEditString.equals("")) {
+                mButton.setOnClickListener(
+                        new View.OnClickListener() {
+                            public void onClick(View view) {
+                                Log.v("EditText", mEdit.getText().toString());
+                                editor.putLong("newSeed", Long.valueOf(mEdit.getText().toString()));
+                                editor.commit();
+                            }
+                        });
+            }
+
         }
 
 }
